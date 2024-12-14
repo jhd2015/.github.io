f3b8d0f169be187d65a41298afb87740a24b6c69
纪海东 <624076529@qq.com>
2024-12-14 14:01:08 +0800

    fix: 2323

--- deploy.sh
+++ deploy.sh
@@ -6,26 +6,26 @@ DEPLOY_BRANCH="page"
 # 打包项目
 npm run build:docs
 
-# 切换到部署分支
-git checkout $DEPLOY_BRANCH
-
 # 拷贝打包后的文件到当前目录
 cp -a dist/. .
 
-# 添加所有更改的文件到 Git
+# 暂存所有更改的文件到 Git
 git add .
 
-# 获取当前时间作为提交信息
-COMMIT_MESSAGE="Deploy build at $(date '+%Y-%m-%d %H:%M:%S')"
+# 保存当前分支的名称
+CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
 
-# 提交更改
-git commit -m "$COMMIT_MESSAGE"
+# 切换到部署分支
+git checkout $DEPLOY_BRANCH
+
+# 应用暂存的更改
+git commit -m "Deploy build at $(date '+%Y-%m-%d %H:%M:%S')"
 
 # 推送到远程仓库
 git push origin $DEPLOY_BRANCH
 
 # 切换回原分支
-git checkout -
+git checkout $CURRENT_BRANCH
 
 # 可以选择删除 dist 目录
-#rm -rf dist
+# rm -rf dist
