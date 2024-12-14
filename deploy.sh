8a1eb6c61116c8ecb8de3e7cb2b2453fb49a9340
纪海东 <624076529@qq.com>
2024-12-14 14:06:12 +0800

    fix: 2323

--- deploy.sh
+++ deploy.sh
@@ -9,23 +9,26 @@ npm run build:docs
 # 拷贝打包后的文件到当前目录
 cp -a dist/. .
 
-# 暂存所有更改的文件到 Git
-git add .
-
-# 保存当前分支的名称
-CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
+# 暂存所有更改的文件到 Git 的 stash
+git stash push -u
 
 # 切换到部署分支
 git checkout $DEPLOY_BRANCH
 
 # 应用暂存的更改
-git commit -m "fix: Deploy build at $(date '+%Y-%m-%d %H:%M:%S')"
+git stash apply
+
+# 提交更改
+git commit -m "Deploy build at $(date '+%Y-%m-%d %H:%M:%S')"
 
 # 推送到远程仓库
 git push origin $DEPLOY_BRANCH
 
 # 切换回原分支
-git checkout $CURRENT_BRANCH
+git checkout -
+
+# 应用原分支的暂存更改
+git stash pop
 
 # 可以选择删除 dist 目录
 # rm -rf dist
